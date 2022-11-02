a3896605226ca017d986da28304f71a9bb975abb
Nicole Ng <115594086+fivefoottwo@users.noreply.github.com>
2022-11-01 23:13:15 -0500

    Add files via upload

---
+++ STAT6324HW3-2B.R
@@ -0,0 +1,106 @@
+#Nicole Ng
+#Part 2/2 of code
+
+#Clear environment
+remove(list = ls())
+
+#load general libraries
+library(dplyr)
+library(ggplot2)
+
+#load analytics libraries
+library(caret)  # confusion matrix and stratified sampling
+library(InformationValue) #missclassification calc
+library(DMwR) #for SMOTE
+
+set.seed(1)
+
+#Set working directory
+setwd("/Users/nicoleng/Dropbox/SMU/MASDA/STAT 6324/data")
+
+#Read csv data
+training <- read.csv("training.csv")
+test <- read.csv("test.csv")
+
+
+
+#model 1
+# final model
+#the goal of model is to improve on the base model
+model1 <- glm(is_canceled ~ ., family = "binomial", data=training)
+
+
+#disable scientific notation for model summary
+options(scipen=999)
+
+#view model summary
+summary(model1)
+
+
+#validate model with test set
+
+#predict using test set
+probabs <- predict(model1, test, type="response")
+
+
+predicted <- ifelse(probabs > 0.5, 1, 0)
+predicted <- factor(predicted)
+
+#note: need to check if the following calc should be probabs or predicted
+#
+
+#confusion matrix
+cm <- confusionMatrix(test$is_canceled, probabs)
+cm
+
+#calculate sensitivity
+sensitivity(test$is_canceled, probabs)
+
+
+#calculate specificity
+specificity(test$is_canceled, probabs)
+
+
+#calculate total misclassification error rate
+misClassError(test$is_canceled, probabs, threshold=0.5)
+
+#AUC and ROC
+plotROC(test$is_canceled, probabs)
+
+
+
+
+ # model 0
+ # base model using variables that were identified by original researchers
+ # note: base model works without one-hot encoding data for training and test sets
+ #       requires a different training and test set without one-hot encoding in file 1
+
+ model0 <- glm(is_canceled ~ meal + stays_in_weekend_nights + stays_in_week_nights +adults + children + assigned_room_type, family = "binomial", data=training)
+
+ #disable scientific notation for model summary
+ options(scipen=999)
+
+ #view model summary
+ summary(model0)
+
+ #test multicollinearity
+ #VIF values above 5 indicate severe multicollinearity
+ 
+ car::vif(model0)
+ 
+
+#predict using test set
+ probabs0 <- predict(model0, test, type="response")
+
+
+ #predicted0 <- ifelse(probabs0 > 0.5, 1, 0)
+ predicted0 <- factor(predicted0)
+
+
+# #confusion matrix
+ cm <- confusionMatrix(test$is_canceled, probabs0)
+ cm
+
+# #AUC and ROC
+ plotROC(test$is_canceled, probabs0)
+
